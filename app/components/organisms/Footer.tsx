8b3d241f670820211d1d0f7bb305ebdf1fab34da
lida Gholamian <ta.gholamian@gmail.com>
2025-01-12 18:03:54 +0330

    [ADD] add structural components [ADD] add base types [ADD] add Button component

---
+++ app/components/organisms/Footer.tsx
@@ -0,0 +1,7 @@
+export const Footer: React.FC = () => {
+  return (
+    <div className="bg-accent">
+      <h1 className="text-3xl">Footer</h1>
+    </div>
+  );
+};
---
+++ app/components/organisms/Header.tsx
@@ -0,0 +1,7 @@
+export const Header: React.FC = () => {
+  return (
+    <div className="bg-accent">
+      <h1 className="text-3xl">Header</h1>
+    </div>
+  );
+};
